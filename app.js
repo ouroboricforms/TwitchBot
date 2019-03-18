d1d28941d86c721e5205850610054924485e1e5a
Lester Caballero <lesterantoniocaballero@gmail.com>
2019-03-18 15:21:38 -0400

    updating.

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 var tim = require('tmi.js');
 
 var quoteArray = [
-"I suck at video games",
-"toebeans are cool",
-"people only talk to me when shroud is offline"
+"Skadoodle originally played with Matt through the Make-A-Wish foundation, Matt wasn't expected to live this long and Skadoodle is in to deep to tell him the truth",
+"Matt secretly plays rap battles on other peoples streams using the alias PAINFULLYWHT because he wishes he was as cool as Lynch",
+"Matt actually uses an aimbot, his is just so bad that not even the bot can compnsate for it 100% of the time",
+"Matt always whiffs",
+"Soba killed Matt's baby just a couple hours after he birthed it",
+"the one with the lactating nipples",
+"Doctors were unable to tell Matts gender when he was born, currently they are still unsure because Matt occasionally lactates when he gets excited.",
+"Matt uses water as syrup on his pannycakes.",
+"Matt pays someone to lower his FPS counter when he’s about to die, so he can always have an excuse",
+"Matt holds a baby pig and other members of Chat hostage in his basement. When enraged, Matt takes the baby pigs blankets away.",
+"Matt holds @Synryc in basement for 3 weeks now without letting him out",
+"Matt used to be best friends with R. Kelly, but due to recent events, they have become business partners.",
+"Matt enjoys studying space because one day he hopes to find an alien, just like as a kid when he hoped to find a friend",
+"Matt has never cut the fingernails on his left hand to allow him to reach more of his keyboard easier.",
+"Matt can show you the world, shining, shimmering splendor.",
+"Matt deleted the polls",
+"Pepega is Matt",
+"Matt has so much game that he doesn't have to flirt with the babes. His chat does it for him.",
+"Matt is so self obsessed that he watches past vods of his own game play.",
+"Matt can slam a revolving door"
 ];
-
-var randomItem = quoteArray[Math.floor(Math.random()*quoteArray.length)];
 
 var options = {
     options: {
@@ -18,20 +33,22 @@ var options = {
     },
     identity: {
         username: "mattsbabiesghost",
-        password: "oauth:v9acr55q5oabors9q89nk8wn0wfrli"
+        password: "oauth:"
     },
-    channels: ["its_wiked"]
+    channels: ["ouroboricform"]
 };
 
 var client = new tim.client(options);
 client.connect();
 
+var randomItem = quoteArray[Math.floor(Math.random() * quoteArray.length) | 0];
+
 client.on('chat', function(channel, user, message, self){
-    if(message === "!mattfacts"){
-        client.action("its_wiked", randomItem)
+    if(message === "!mattfacts") {
+        client.say("ouroboricform", randomItem)
     }
 });
 
 client.on('connected', function(address, port){
-    client.action("its_wiked", "Hell I'm matts dead baby, and you are a scrub. Mod me Matt");
+    client.say("ouroboricform", "Hello I'm Matt's dead baby, and you are a scrub.");
 });
