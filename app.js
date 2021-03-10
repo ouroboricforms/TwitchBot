b012890b3d4193d08a5bde8b233198ca6ca62f27
Lester Caballero <ouroboricforms@gmail.com>
2021-03-10 00:01:23 -0500

    hiding api keys

--- app.js
+++ app.js
@@ -1,54 +1,86 @@
-var tim = require('tmi.js');
-
-var quoteArray = [
-" #1 Skadoodle originally played with Matt through the Make-A-Wish foundation, Matt wasn't expected to live this long and Skadoodle is in to deep to tell him the truth",
-" #2 Matt secretly plays rap battles on other peoples streams using the alias PAINFULLYWHT because he wishes he was as cool as Lynch",
-" #3 Matt actually uses an aimbot, his is just so bad that not even the bot can compnsate for it 100% of the time",
-" #4 Matt always whiffs",
-" #5 Soba killed Matt's baby just a couple hours after he birthed it",
-" #6 the one with the lactating nipples",
-" #7 Doctors were unable to tell Matts gender when he was born, currently they are still unsure because Matt occasionally lactates when he gets excited.",
-" #8 Matt uses water as syrup on his pannycakes.",
-" #9 Matt pays someone to lower his FPS counter when he’s about to die, so he can always have an excuse",
-" #10 Matt holds a baby pig and other members of Chat hostage in his basement. When enraged, Matt takes the baby pigs blankets away.",
-" #11 Matt holds @Synryc in basement for 3 weeks now without letting him out",
-" #12 Matt used to be best friends with R. Kelly, but due to recent events, they have become business partners. ",
-" #13 Matt enjoys studying space because one day he hopes to find an alien, just like as a kid when he hoped to find a friend",
-" #14 Matt has never cut the fingernails on his left hand to allow him to reach more of his keyboard easier.",
-" #15 Matt can show you the world, shining, shimmering splendor.",
-" #16 Matt deleted the polls",
-" #17 Pepega is Matt",
-" #18 Matt has so much game that he doesn't have to flirt with the babes. His chat does it for him.",
-" #19 Matt is so self obsessed that he watches past vods of his own game play.",
-" #20 Matt can slam a revolving door"
-];
-
+var tim = require("tmi.js");
+var quote = require("./quoteArray")
+//dotenv to manage your EVN variables in NodeJS
+require('dotenv').config();
+let password = process.env.OAUTH_KEY;
+let username = process.env.USERNAME;
 var options = {
-    options: {
-        debug: true
-    },
-    connection: {
-        cluster: "aws",
-        reconnect: true
-    },
-    identity: {
-        username: "",
-        password: "oauth:"
-    },
-    channels: ["its_wiked"]
+  options: {
+    debug: true
+  },
+  connection: {
+    cluster: "aws",
+    reconnect: true
+  },
+  identity: {
+    username: username,
+    password: password,
+  },
+  channels: ["ouroboricform"]
 };
 
 var client = new tim.client(options);
 client.connect();
 
-
-client.on('chat', function(channel, user, message, self){
-    var randomItem = quoteArray[Math.floor(Math.random() * quoteArray.length) | 0];
-    if(message === "!mattfacts") {
-        client.say("its_wiked", randomItem)
-    }
+client.on("chat", function(channel, user, message, self) {
+  var randomItem =
+    quote.quoteArray[Math.floor(Math.random() * quote.quoteArray.length) | 0];
+  if (message === "!mattfacts") {
+    client.say("ouroboricform", randomItem);
+  }
 });
 
-client.on('connected', function(address, port){
-    client.say("its_wiked", "Hello I'm Matt's dead baby. please type !mattfacts to know more about what this monster has done.");
+client.on("connected", function(address, port) {
+  client.say(
+    "ouroboricform",
+    `Reinforcements have arrived.`
+  );
+});
+
+client.on("chat", function(channel, userstate, message, self) {
+  if (message === "!hello") {
+    if (userstate["display-name"]) {
+      client.say("ouroboricform", `${userstate.username}, whats up bro?`);
+    }
+  }
+});
+
+client.on("whisper", (from, userstate, message, self) => {
+  if (self) {
+    return;
+  } else if (message === "!yes") {
+    client.say(
+      "ouroboricform",
+      `${
+        userstate.username
+      } has voted yes! FeelsGoodMan `
+    );
+  } else if (message === "!no") {
+    client.say(
+      "ouroboricform",
+      `${
+        userstate.username
+      } has voted no! FeelsBadMan `
+    );
+  }
+});
+
+client.on("whisper", (from, userstate, message, self) => {
+  if (self) {
+    return;
+  } else if (message === "!terrible") {
+    client.say(
+      "ouroboricform",
+      `${
+        userstate.username
+      } has said you're a terrible gamer! FeelsGoodMan `
+    );
+  } else if (message === "!awful") {
+    client.say(
+      "ouroboricform",
+      `${
+        userstate.username
+      } has voted you're an awful streamer! FeelsBadMan `
+    );
+  }
 });
---
+++ quoteArray.js
@@ -0,0 +1,25 @@
+var quoteArray = [
+    " #1 Skadoodle originally played with Matt through the Make-A-Wish foundation, Matt wasn't expected to live this long and Skadoodle is in to deep to tell him the truth",
+    " #2 Matt secretly plays rap battles on other peoples streams using the alias PAINFULLYWHT because he wishes he was as cool as Lynch",
+    " #3 Matt actually uses an aimbot, his is just so bad that not even the bot can compnsate for it 100% of the time",
+    " #4 Matt always whiffs",
+    " #5 Soba killed Matt's baby just a couple hours after he birthed it",
+    " #6 the one with the lactating nipples",
+    " #7 Doctors were unable to tell Matts gender when he was born, currently they are still unsure because Matt occasionally lactates when he gets excited.",
+    " #8 Matt uses water as syrup on his pannycakes.",
+    " #9 Matt pays someone to lower his FPS counter when he’s about to die, so he can always have an excuse",
+    " #10 Matt holds a baby pig and other members of Chat hostage in his basement. When enraged, Matt takes the baby pigs blankets away.",
+    " #11 Matt holds @Synryc in basement for 3 weeks now without letting him out",
+    " #12 Matt used to be best friends with R. Kelly, but due to recent events, they have become business partners. ",
+    " #13 Matt enjoys studying space because one day he hopes to find an alien, just like as a kid when he hoped to find a friend",
+    " #14 Matt has never cut the fingernails on his left hand to allow him to reach more of his keyboard easier.",
+    " #15 Matt can show you the world, shining, shimmering splendor.",
+    " #16 Matt deleted the polls",
+    " #17 Pepega is Matt",
+    " #18 Matt has so much game that he doesn't have to flirt with the babes. His chat does it for him.",
+    " #19 Matt is so self obsessed that he watches past vods of his own game play.",
+    " #20 Matt can slam a revolving door",
+    `" #21 Matt is Mendokusaii if Mendokusaii was bad at games" - added by Ouroboricform`
+  ];
+
+  module.exports = {quoteArray};
